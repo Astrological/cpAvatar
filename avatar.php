fcae9a61e1313f590d91fc3c5f377a6afb9c1b55
Astrological <karl.the.wild@gmail.com>
2014-05-22 05:38:33 -0700

    Update avatar.php
    'avatar.php' base changed.

--- avatar.php
+++ avatar.php
@@ -13,33 +13,18 @@ header('Content-Type: image/png');
 header('Cache-Control: max-age=31556926');
 
 class penguinAvatar{
-  private static $avatarArr = array();
-  private static $avatarSize = null;
+  // More will be added soon
   
-  public function __construct($avatarArr, $avatarSize){
-    // Set the avatar array
-    self::$avatarArr = $avatarArr;
-    
-    // Set avatar size
-    self::$avatarSize = $avatarSize;
-    
-    self::createAvatar();
-  }
-  
-  private function createAvatar(){
-    $avatarMain = imagecreatefrompng('http://media1.clubpenguin.com/avatar/paper/' . self::$avatarSize . '/' . self::$avatarArr[0] . '.png');
+  private function createAvatar(array $avatarArr = [], $avatarSize){
+    $avatarMain = imagecreatefrompng('http://media1.clubpenguin.com/avatar/paper/' . $avatarSize . '/' . $avatarArr[0] . '.png');
     
     imagesavealpha($avatarMain, true);
     
-    foreach(self::$avatarArr as $avatarItem){
-      if($avatarItem !== self::$avatarArr[0]){
-        $avatarMainItem = imagecreatefrompng('http://media1.clubpenguin.com/avatar/paper/' . self::$avatarSize . '/' . $avatarItem . '.png');
+    foreach($avatarArr as $avatarItem){
+      if($avatarItem !== $avatarArr[0]){
+        $avatarMainItem = imagecreatefrompng('http://media1.clubpenguin.com/avatar/paper/' . $avatarSize . '/' . $avatarItem . '.png');
         
-        imagecopy($avatarMain, $avatarMainItem, 0, 0, 0, 0, self::$avatarSize, self::$avatarSize);
-        
-        // Flush
-        flush();
-        ob_flush();
+        imagecopy($avatarMain, $avatarMainItem, 0, 0, 0, 0, $avatarSize, $avatarSize);
       }
     }
     
@@ -51,7 +36,8 @@ class penguinAvatar{
 $avatarArr = explode('|', $_GET['avatarInfo']);
 $avatarSize = $_GET['avatarSize'];
 
-// Call the penguinAvatar class
-new penguinAvatar($avatarArr, $avatarSize);
+// Call the penguinAvatar class & Create the avatar image
+$penguinAvatar = new penguinAvatar();
+$penguinAvatar->createAvatar($avatarArr, $avatarSize);
 
 ?>
